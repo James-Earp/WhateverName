31279f37f8b494a3f4c0bdeb86b0fe09cad4fa0e
James Earp <JamesAEarp@outlook.com>
2020-11-06 11:13:20 -0600

    initial commit - created solution and UI project

---
+++ WhateverName.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhateverName.UI.MVC.Startup))]
+namespace WhateverName.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
