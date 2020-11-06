fe089a0fe8c9ca14185b61a192b57fa12a336471
James Earp <JamesAEarp@outlook.com>
2020-11-06 11:34:02 -0600

    Implemented Identity Samples. - replaced stuff

--- WhateverName.UI.MVC/Startup.cs
+++ WhateverName.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(WhateverName.UI.MVC.Startup))]
 namespace WhateverName.UI.MVC
 {
     public partial class Startup
